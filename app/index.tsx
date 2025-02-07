2bbd470a09707875f72f263c0343aea23ae7adab
kaikaci12 <somewonesomewone@gmail.com>
2025-02-07 15:10:06 +0400

    add background video

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,52 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, Pressable, StyleSheet, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Video } from "expo-av";
+const videoSource = require("../assets/videos/7579667-uhd_2160_4096_25fps.mp4");
 
 const Page = () => {
+  // Initialize the video player with looping and autoplay enabled
+
   return (
-    <View>
-      <SafeAreaView>
-        <Text>Hello world</Text>
-      </SafeAreaView>
+    <View style={styles.container}>
+      <Pressable style={styles.login}>Login</Pressable>
+      <Video
+        isLooping
+        source={videoSource}
+        isMuted
+        style={styles.video}
+        shouldPlay
+      />
     </View>
   );
 };
 
 export default Page;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+  },
+  login: {
+    display: "flex",
+    width: 111,
+    height: 26,
+    flexDirection: "column",
+    justifyContent: "center",
+    color: "#FFF",
+    backgroundColor: "transparent",
+    textAlign: "center",
+
+    fontSize: 12,
+    fontStyle: "normal",
+    fontWeight: "700",
+    lineHeight: 12,
+    borderColor: "white",
+    borderWidth: 1,
+  },
+});
